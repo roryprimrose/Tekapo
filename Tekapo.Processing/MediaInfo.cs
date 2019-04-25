509e52acd98c48c9e03a39e16f63dc9d5aab66a5
Rory Primrose <rory@watchium.com>
2019-04-25 19:55:22 +1000

    Added MediaInfo and extension method to create it from a file path.

---
+++ Tekapo.Processing/MediaInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Tekapo.Processing
+{
+    using System;
+
+    public class MediaInfo
+    {
+        public string FilePath { get; set; }
+
+        public string Hash { get; set; }
+
+        public DateTime? MediaCreated { get; set; }
+    }
+}
---
+++ Tekapo.Processing/MediaManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Tekapo.Processing
+{
+    using System.IO;
+
+    public static class MediaManagerExtensions
+    {
+        public static MediaInfo ReadMediaInfo(IMediaManager mediaManager, string filePath)
+        {
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var mediaCreated = mediaManager.ReadMediaCreatedDate(stream);
+                var hash = stream.CalculateHash();
+
+                return new MediaInfo {FilePath = filePath, Hash = hash, MediaCreated = mediaCreated};
+            }
+        }
+    }
+}
